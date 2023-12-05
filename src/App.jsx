f7538a616018bdead780e93cede1b8cdb80c9c2d
Abubakar Jilani <abub1156@gmail.com>
2023-12-05 12:27:52 +0500

    fix issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import {Route, Routes } from 'react-router-dom' 
-import Home from './Components/Home'
-import Viewall from './Components/Viewall'
-import Adduser from './Components/Adduser'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Viewall from "./Components/Viewall";
+import Adduser from "./Components/Adduser";
 const App = () => {
   return (
     <>
-       <Routes>
-      <Route path='/' element={<Home/>} exact />
-      <Route path='/add' element={<Adduser/>} exact />
-      <Route path='/view' element={<Viewall/>} exact />
-
-    </Routes> 
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/add" element={<Adduser />} exact />
+        <Route path="/view" element={<Viewall />} exact />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
