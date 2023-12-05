cf88f41e7df83ab4c881434ff9743569259c693a
Adil <react631@gmail.com>
2023-12-05 17:08:25 +0500

    back button

--- src/Components/Adduser.jsx
+++ src/Components/Adduser.jsx
@@ -34,6 +34,10 @@ const Adduser = () => {
 
   };
 
+  const go_back = () => {
+    navigate('/')
+  }
+
   const handle_submit = async (event) => {
     event.preventDefault();
 
@@ -64,45 +68,54 @@ const Adduser = () => {
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-slate-700">
-      <h1 className="text-3xl text-center text-yellow-600">Add User</h1>
+      <h1 className="text-3xl text-center text-white">Add User</h1>
       <div className="flex flex-col w-1/2 p-8 mt-5 rounded-lg bg-slate-500">
         <form>
           <input
             type="text"
             placeholder="name"
             autoComplete="off"
-            className="w-full p-3 text-yellow-600 placeholder-yellow-600 bg-transparent border-2 border-yellow-600 rounded-md outline-none"
+            className="w-full p-3 mt-1 text-white placeholder-white bg-transparent border-2 border-white rounded-md outline-none"
             name="name"
             value={input.name}
             onChange={input_value}
           />
-           {errors.name && <div className="mb-5 text-red-500">{errors.name}</div>}
+           {errors.name && <div className="mb-2 text-red-500">{errors.name}</div>}
           <input
             type="text"
             placeholder="Location "
             autoComplete=""
-            className="w-full p-3 text-yellow-600 placeholder-yellow-600 bg-transparent border-2 border-yellow-600 rounded-md outline-none"
+            className="w-full p-3 mt-3 text-white placeholder-white bg-transparent border-2 border-white rounded-md outline-none"
             name="address"
             value={input.address}
             onChange={input_value}
           />
- {errors.address && <div className="mb-5 text-red-500">{errors.address}</div>}
+ {errors.address && <div className="mb-2 text-red-500">{errors.address}</div>}
           <input
             placeholder="Enter Mobile Number"
             autoComplete="new-password"
-            className="w-full p-3 text-yellow-600 placeholder-yellow-600 bg-transparent border-2 border-yellow-600 rounded-md outline-none mb-"
+            className="w-full p-3 mt-3 text-white placeholder-white bg-transparent border-2 border-white rounded-md outline-none mb-"
             type="text"
             name="mobile"
             value={input.mobile}
             onChange={input_value}
           />
-           {errors.mobile && <div className="mb-5 text-red-500">{errors.mobile}</div>}
-          <button
+           {errors.mobile && <div className="mb-2 text-red-500">{errors.mobile}</div>}
+
+          <div className="flex justify-around w-full mt-3">
+          <button 
             onClick={handle_submit}
-            className="w-1/2 px-8 py-2 text-white bg-yellow-600 border border-yellow-600 rounded hover:text-white hover:border-transparent"
+            className="w-1/3 px-8 py-2 mt-3 bg-white border border-white rounded text-slate-900 hover:text-white hover:border-transparent"
           >
             Add New
           </button>
+          <button 
+            onClick={go_back}
+            className="w-1/3 px-8 py-2 mt-3 bg-white border border-white rounded text-slate-900 hover:text-white hover:border-transparent"
+          >
+          Go Back
+          </button>
+          </div>
         </form>
       </div>
     </div>
